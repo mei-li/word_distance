e035bacb6f862ac85a3f19c7a7ee65b8c49c4464
mei-li <meili.tr@gmail.com>
2018-10-15 09:30:09 +0200

    test word_distance corner cases

--- test_word_distance.py
+++ test_word_distance.py
@@ -13,3 +13,18 @@ def test_word_distance_multiple():
 
 def test_word_distance_double_end():
     assert word_distance(['a', 'black', 'box', 'is', 'in', 'that', 'box'], 'a', 'box') == 1
+
+
+@pytest.mark.parametrize('word1, word2',[
+    ('a', 'not_there'),
+    ('not_there', 'box'),
+    (None, None),
+])
+def test_word_distance_not_found(word1, word2):
+    assert word_distance(['a', 'black', 'box'], word1, word2) is None
+
+
+def test_word_distance_integration():
+    assert word_distance("We do value and reward motivation in our development team. "
+                         "Development is a key skill for a DevOp.".split(), 'motivation',
+                         'development') == 2
